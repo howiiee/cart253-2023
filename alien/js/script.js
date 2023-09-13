b6bfd373f325df20058d7427d47017adcd7617c2
howiiee <96639739+howiiee@users.noreply.github.com>
2023-09-13 06:26:37 -0400

    Update script.js

--- alien/js/script.js
+++ alien/js/script.js
@@ -20,8 +20,42 @@ function preload() {
  * Description of setup
 */
 function setup() {
+
     createCanvas(400,400);
     background(0, 0, 0);
+    angleMode(DEGREES);
+    noStroke();
+    
+    // head
+    fill(150, 173, 147);
+    rectMode(CENTER);
+    ellipse(200, 170, 200, 170);
+    // left cheek
+    ellipse(165, 195, 100, 120);
+    // right cheek
+    ellipse(235, 195, 100, 120);
+    //chin
+    ellipse(200, 200, 120, 200);
+
+    // left eye
+    fill(0, 0, 0);
+    ellipse(150, 180, 90, 70);
+
+    // right eye
+    fill(0, 0, 0);
+    ellipse(250, 180, 90, 70);
+
+    // left nostril
+    fill(0, 0, 0);
+    ellipse(190, 230, 6, 15);
+
+    // right nostril
+    fill(0, 0, 0);
+    ellipse(210, 230, 6, 15);
+
+    // mouth
+    
+
 }
 
 
