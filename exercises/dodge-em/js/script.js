43d950c13612976e81246a4e2a248f2b986d21a5
howie <dufresnehoward@icloud.com>
2023-09-28 17:40:19 -0400

    added comments

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -2,6 +2,9 @@
 let bgGif;
 let fire;
 
+
+// Define celestial bodies with initial properties
+// sun object representing the Sun with its coordinates, dimensions, and image
 let sun = {
     x: 250,
     y: 250,
@@ -11,6 +14,7 @@ let sun = {
 
 }
 
+// Define other celestial bodies similarly, with initial properties and dragging states
 let uranus = {
     x: 250,
     y:95,
@@ -103,17 +107,18 @@ let planets = [uranus, saturn, alien, planet, planet2, moon, mars, earth];
 
 function preload() {
 
-bgGif = loadImage("assets/images/space.gif");
-sun.img = loadImage("assets/images/sun.gif");
-earth.img = loadImage("assets/images/earth.gif");
-uranus.img = loadImage("assets/images/uranus.png");
-planet.img = loadImage("assets/images/planet.png");
-planet2.img = loadImage("assets/images/planet2.gif");
-mars.img = loadImage("assets/images/mars.png");
-saturn.img = loadImage("assets/images/saturn.png");
-alien.img = loadImage("assets/images/alien.png");
-moon.img = loadImage("assets/images/moon.png");
-fire = loadImage("assets/images/fire.gif");
+    // Load background, sun, planets, and fire images
+    bgGif = loadImage("assets/images/space.gif");
+    sun.img = loadImage("assets/images/sun.gif");
+    earth.img = loadImage("assets/images/earth.gif");
+    uranus.img = loadImage("assets/images/uranus.png");
+    planet.img = loadImage("assets/images/planet.png");
+    planet2.img = loadImage("assets/images/planet2.gif");
+    mars.img = loadImage("assets/images/mars.png");
+    saturn.img = loadImage("assets/images/saturn.png");
+    alien.img = loadImage("assets/images/alien.png");
+    moon.img = loadImage("assets/images/moon.png");
+    fire = loadImage("assets/images/fire.gif");
 
 }
 
@@ -124,16 +129,21 @@ function setup() {
 
 
 function draw() {
+    // Draw the background gif and the sun at their respective positions
     image(bgGif, 250, 250, width, height);
     image(sun.img, sun.x, sun.y, sun.width, sun.height);
 
+    // Loop through each planet in the planets array
     for (let i = 0; i < planets.length; i++) {
+       // If the planet is being dragged, update its position to the mouse coordinates
         if (planets[i].dragging) {
             planets[i].x = mouseX;
             planets[i].y = mouseY;
         }
         
         let d = dist(sun.x, sun.y, planets[i].x, planets[i].y);
+        
+        // If a planet is too close to the sun, draw a fire image at the planet’s position
         if (d < sun.width / 2 + planets[i].width / 2) {
             image(fire, planets[i].x, planets[i].y, planets[i].width, planets[i].height);
         } else {
@@ -143,6 +153,7 @@ function draw() {
 }
 
 
+// Mouse pressed function to check if a planet is selected for dragging
 function mousePressed(){
 
     for (let i = 0; i < planets.length; i++){
@@ -154,8 +165,11 @@ function mousePressed(){
 
 }
 
+// Mouse released function to reset the planets to their initial positions and stop dragging
 function mouseReleased() {
     for (let i = 0; i < planets.length; i++) {
+        
+        // If the planet was being dragged, reset its position and set dragging to false
         if(planets[i].dragging) {
             planets[i].x = planets[i].initialX;
             planets[i].y = planets[i].initialY;
