3b7c70d4496ab60dc8ddaadac9959212ced94e9d
howie <dufresnehoward@icloud.com>
2023-09-28 09:00:56 -0400

    Update script.js

--- conditionals/mouse input/js/script.js
+++ conditionals/mouse input/js/script.js
@@ -1,32 +1,30 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
-
-"use strict";
-
-/**
- * Description of preload
-*/
-function preload() {
-
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100
 }
 
+let bg = {
+    r:0,
+    g:0,
+    b:0
+}
 
-/**
- * Description of setup
-*/
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
-/**
- * Description of draw()
-*/
+
 function draw() {
+    background(bg.r, bg.g, bg.b);
+
+    ellipse(circle.x,circle.y,circle.size);
+}
+
+function mouseMoved(){
+    bg.r = random(0, 255);
+    bg.g = random(0, 255);
+    bg.b = random(0, 255);
 
 }
