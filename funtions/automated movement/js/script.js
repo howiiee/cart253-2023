9a4d151fbf8608dbb740357c3df2b07da1d176c5
howie <dufresnehoward@icloud.com>
2023-10-02 16:55:58 -0400

    Update script.js

--- funtions/automated movement/js/script.js
+++ funtions/automated movement/js/script.js
@@ -1,32 +1,72 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+let circle = {
+    x:250,
+    y: 250,
+    size: 100,
+    vx:0,
+    vy: 0,
+    speed: 2,
+};
 
-"use strict";
-
-/**
- * Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
- * Description of setup
-*/
 function setup() {
+    createCanvas(500, 500);
 
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
 
+    background(0);
+
+    // maybe shivering, maybe somthing about to explose
+    // circle.vx = random(-circle.speed, circle.speed);
+    // circle.vy = random(-circle.speed, circle.speed); 
+    
+
+    //maybe the circle is searchign for something
+    // let change = random();
+    // if (change < 0.05){
+    //     circle.vx = random(-circle.speed, circle.speed);
+    //     circle.vy = random(-circle.speed, circle.speed); 
+    // }
+
+    // following the mouse
+    // let dx = circle.x - mouseX;
+    // let dy = circle.y - mouseY;
+
+    // if (dx<0){
+    //     circle.vx = circle.speed;
+    // }
+    // else if (dx>0) {
+    //     circle.vx = -circle.speed;
+    // }
+
+    // if (dy<0){
+    //     circle.vy = circle.speed;
+    // }
+    // else if (dy>0) {
+    //     circle.vy = -circle.speed;
+    // }
+
+    // fleeing the mouse
+    let dx = circle.x - mouseX;
+    let dy = circle.y - mouseY;
+
+    if (dx<0){
+        circle.vx = -circle.speed;
+    }
+    else if (dx>0) {
+        circle.vx = circle.speed;
+    }
+
+    if (dy<0){
+        circle.vy = -circle.speed;
+    }
+    else if (dy>0) {
+        circle.vy = circle.speed;
+    }
+ 
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    ellipse(circle.x,circle.y,circle.size);
 }
